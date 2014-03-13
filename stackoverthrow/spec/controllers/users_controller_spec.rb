fe5a564bd16ee0cebc9f59b450b16bd3ef593324
José Menor & Ivan Sued <pair+menor.thefenry@devbootcamp.com>
2014-03-13 16:25:00 -0700

    Add controller tests skeleton for the user controller

--- stackoverthrow/spec/controllers/users_controller_spec.rb
+++ stackoverthrow/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,34 @@
- require 'test_helper'
+ require 'spec_helper'
 
-class UsersControllerTest < ActionController::TestCase
+describe UsersController do
+  describe '#show' do
+    it "should show user profile"
+  end
 
+  describe '#new' do
+    context 'logged in' do
+      it "should redirect the user to their profile"
+    end
+    context 'not logged in' do
+      it "should show a new user form"
+    end
+  end
+
+  context '#create' do
+    it "should add a new user to the database"
+  end
+
+  describe '#edit' do
+    context "user is logged in" do
+      it "should show an edit form"
+    end
+
+    context 'user is not logged in' do
+      it "should redirect to login page"
+    end
+  end
+
+  describe '#update' do
+    it "should update a user details"
+  end
 end
