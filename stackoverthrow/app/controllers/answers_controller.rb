e414b91bd115521ccdb556ba24afa9a2aaf8f09e
Darren Dahl & Brittany Mazza <pair+darrendahl.ladymozzarella@devbootcamp.com>
2014-03-13 19:28:57 -0700

    Implements test for new answer and it works and stuff

--- stackoverthrow/app/controllers/answers_controller.rb
+++ stackoverthrow/app/controllers/answers_controller.rb
@@ -1,3 +1,10 @@
 class AnswersController < ApplicationController
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+  end
 
+  def create
+
+  end
 end
--- stackoverthrow/spec/controllers/answers_controller_spec.rb
+++ stackoverthrow/spec/controllers/answers_controller_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe AnswersController do
-  describe "#new" do
+  let!(:answer){ create :answer }
+  let!(:question){ create :question }
 
+  describe "#new" do
+    it "should be successful" do
+      get :new, :question_id => question.id
+      expect(response).to be_success
+    end
+
+    it "should assign an instance" do
+      get :new, :question_id => question.id
+      expect(assigns(:answer)).to be_a_new Answer
+    end
   end
 
   describe "#create" do
