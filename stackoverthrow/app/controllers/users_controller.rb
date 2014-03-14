1f59d9d8265496ed03b60b2501ceb37ef57596da
José Menor & Ivan Sued <pair+menor.thefenry@devbootcamp.com>
2014-03-13 18:04:14 -0700

    Add working show route. Tests pass

--- stackoverthrow/app/controllers/users_controller.rb
+++ stackoverthrow/app/controllers/users_controller.rb
@@ -7,13 +7,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to new_question_path
+      session[:id] = @user.id
+      redirect_to root_path
     else
       render 'users/new'
     end
   end
 
   def show
+    redirect_to new_user_path unless session[:id]
     @user = User.find(params[:id])
   end
 
