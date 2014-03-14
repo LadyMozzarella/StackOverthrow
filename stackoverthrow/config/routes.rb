947b2ef4fb6292d1b980eca7d5ea5ebf832fec04
José Menor & Ivan Sued <pair+menor.thefenry@devbootcamp.com>
2014-03-13 19:23:40 -0700

    Add logout functionality Still missing login functionality

--- stackoverthrow/config/routes.rb
+++ stackoverthrow/config/routes.rb
@@ -2,6 +2,5 @@ Stackoverthrow::Application.routes.draw do
   root :to => "questions#index"
   resources :questions
 
-  resources :users, only: [:show, :create, :new]
-
+  resources :users, only: [:show, :create, :new, :destroy]
 end
