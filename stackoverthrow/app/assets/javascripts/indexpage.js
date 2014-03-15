8c7fd37d73c7713abe0f574cc6bb269449bbc54f
thefenry <thefenry@gmail.com>
2014-03-15 11:36:11 -0700

    This is my spike JQuery is active but I have not implemented anything

---
+++ stackoverthrow/app/assets/javascripts/indexpage.js
@@ -0,0 +1,27 @@
+var Navigation = {
+  bindEvents: function() {
+    $('.new_question').on('submit', this.appendQuestion);
+    // $('.nav-tabs.units').on('click', 'li a', this.showTab);
+    // $('.nav-tabs.content').on('click', 'li a', this.showContent);
+  },
+  appendQuestion: function(){
+    event.preventDefault();
+  }
+  // showTab: function() {
+  //   event.preventDefault();
+  //   $('.tab-pane').removeClass('active');
+  //   var contentLink = $(this).data('toggle');
+  //   $(contentLink).addClass('active');
+  // },
+
+  // showContent: function() {
+  //   event.stopPropagation();
+  //   $(this).siblings('.challenge-content').toggleClass('hidden');
+  // }
+}
+
+$(document).ready(function() {
+  Navigation.bindEvents();
+})
+
+
