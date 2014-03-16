cc48419f2c05e3057319d287c302c70c943eeb20
thefenry <thefenry@gmail.com>
2014-03-15 20:21:54 -0700

    Fix JS so that the forms are appended to the grandparent

--- stackoverthrow/app/assets/javascripts/indexpage.js
+++ stackoverthrow/app/assets/javascripts/indexpage.js
@@ -6,12 +6,12 @@ var Navigation = {
   
   appendLoginForm: function(evt, data){
     var form = $(data).find('form');
-    $('.login_btn').after(form);
+    $('.login_btn').parent().parent().after(form);
   },
 
   appendSignUp: function(evt, data){
     var form = $(data).find('form');
-    $('.signup_btn').after(form);
+    $('.signup_btn').parent().parent().after(form);
   }
 };
 
