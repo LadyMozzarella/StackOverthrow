1eb46c6bbb7dcfcb61aac2fe895ac78757b893cc
thefenry <thefenry@gmail.com>
2014-03-16 17:22:51 -0700

    Add all the comment functionality to the questions and answers

--- stackoverthrow/app/controllers/comments_controller.rb
+++ stackoverthrow/app/controllers/comments_controller.rb
@@ -10,22 +10,21 @@ class CommentsController < ApplicationController
 		@comment.user_id=session[:id]
 		if @comment.save
 			flash[:notice] = "Successfully created comment."
-			redirect_to @commentable
+			if @commentable.class.name == "Answer"
+				redirect_to @commentable.question
+			else
+				redirect_to @commentable
+			end
 		else
 			render :action => 'new'
 		end
 	end
 
-
 	private
 
 	def load_commentable
-    	# klass = [Question, Answer].detect { |v| params["#{v.name.underscore}_id"]}
-    	# @votable = klass.find(params["#{klass.name.underscore}_id"])
-
     	params.each do |name, value|
 			return $1.classify.constantize.find(value) if name =~ /(.+)_id$/
-			
 		end
   	end
 end
