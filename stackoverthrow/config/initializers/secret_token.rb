d41d51a4aeaccd3df429b34d53c82eb7d4466452
thefenry <thefenry@gmail.com>
2014-04-04 13:34:27 -0700

    Move .git to application

--- stackoverthrow/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Stackoverthrow::Application.config.secret_token = 'abcdc08454c42d73bb1ffd7490288d3ddf23e9b34a1f72d97d791e296a0f1be5424992bd21dcb520a5f89ff78f0315364da17c868c3da333fea15bc3f3224dc2'
